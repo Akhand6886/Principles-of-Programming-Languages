17fb08ebb3b1c774658e7b6f1112980c07eb97a3
Akhand Pratap Singh <Aksingh6886@gmail.com>
2021-10-23 10:39:13 +0530

    2D Arrays and searching techniques

---
+++ Lab 7/SUM OF  ALL THE ELEENTS IN AN ARRAY exp 7.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int  i ,j ,sum=0;
+int main ()
+{
+
+int array[10];
+	printf("Enter 10 elemtnts in the array");
+	
+	for(i=0;i < 10 ; i++)
+{
+	scanf("%d", &array[i]);    
+}
+for(j=0 ; j < 10 ; j++)
+ {
+ 
+ sum = sum + array[j] ;
+
+}
+printf("sum of the array  = %d",sum);
+ return 0;
+}
